0ac245000a4cf78f2a0486864fc806ab8649b69f
Simon Rintala <siimon_7@icloud.com>
2025-01-13 13:06:29 +0100

    Added packages, project start

---
+++ src/main/java/com/example/uppgift_backend/backend_uppgift1/BackendUppgift1Application.java
@@ -0,0 +1,13 @@
+package com.example.uppgift_backend.backend_uppgift1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendUppgift1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendUppgift1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/uppgift_backend/backend_uppgift1/BackendUppgift1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uppgift_backend.backend_uppgift1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendUppgift1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
